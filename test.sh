12641ad83bf657e6aed92dbe7345307598463d88
ishowta <kinironote@gmail.com>
2018-11-09 13:27:43 +0900

    [clean] update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-rm -rf tmp/data_test
-cp -r data_test tmp/data_test
-python3 generate.py --data_path="data_test" /contests/tenka1-2018 /contests/tenka1-2018-beginner
-rm -rf data_test
-mv tmp/data_test data_test
+rm -rf tmp/data
+cp -r test/data tmp/data
+python3 generate.py --data_path="tmp/data" /contests/tenka1-2018 /contests/tenka1-2018-beginner
