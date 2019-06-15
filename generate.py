17b8daab440b55b5446c81d357e0d91b7b6f3dc4
ishowta <ishowta@gmail.com>
2019-06-15 19:09:40 +0900

    user_listが存在しなかった場合に前回のユーザーリストを現在のユーザーリストで初期化してしまっていたので修正

--- generate.py
+++ generate.py
@@ -342,7 +342,8 @@ if __name__ == '__main__':
         exit()
 
     # 前回のユーザー情報のロード
-    pre_user_list = pickle.load(open(user_list_path, 'rb')) if os.path.exists(user_list_path) else user_list
+    pre_user_list = pickle.load(open(user_list_path, 'rb')) if os.path.exists(
+        user_list_path) else pd.DataFrame(index=['color', 'count', 'name', 'rating', 'rank'])
 
     # レートが更新されるまで待つ
     logger.info('Wait rating update')
